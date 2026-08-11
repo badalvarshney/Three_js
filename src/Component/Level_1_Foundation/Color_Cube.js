8260ad806a2b54fc9da516ae513006edba864a3f
Badal Varshney <varshneybadal4@gmail.com>
2026-08-11 17:44:37 +0530

    Refactor ColorCube component: adjust camera position, update cube geometry, and enhance animation loop

--- src/Component/Level_1_Foundation/Color_Cube.js
+++ src/Component/Level_1_Foundation/Color_Cube.js
@@ -9,23 +9,46 @@ export default function ColorCube() {
 		0.1,
 		1000
 	);
-	camera.position.z = 5;
+	camera.position.z = 3;
 	const renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
-	renderer.setClearColor(0x000555);
+	renderer.setClearColor(0x000000);
 	document.body.appendChild(renderer.domElement)
-	
+
 	// Light
 	const ambientLight = new THREE.AmbientLight(0xffffff, 2);
 	scene.add(ambientLight);
 	const dirLight = new THREE.DirectionalLight(0xffffff, 3);
 	scene.add(dirLight);
-	
-	
+
+
 	// Geomatry
-	const cubeGeometry = new THREE.BoxGeometry(2,2);
+	const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
 	const cubeMaterial = new THREE.MeshStandardMaterial();
 	const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-	scene.add(cube)
-	renderer.render(scene, camera);
+	scene.add(cube);
+
+	let hue = 0;
+
+
+	function animate() {
+		requestAnimationFrame(animate)
+		hue += 0.001
+		// Cube Rotation X,Y,Z 
+		cube.rotation.x += 0.01;
+		cube.rotation.y += 0.02;
+		cube.rotation.z += 0.01;
+
+		// Cube Color 
+		console.log(hue);
+		
+		if (hue >= 1) {
+			hue = 0;
+		}
+		cubeMaterial.color.setHSL(hue, 1, 0.5);
+
+		// Render Render
+		renderer.render(scene, camera);
+	}
+	animate();
 }
